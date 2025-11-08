f0cf4389dfbad946c10dcd1b780722fa67677299
Viswanadh Vunnamatla <viswanadhvunnamatla@gmail.com>
2025-11-08 10:48:11 +0530

    updated cors allowed origins

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,25 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-// middlewares
-const allowedOrigins = ["*"];
+const allowedOrigins = [
+  "https://bookstore855.netlify.app",
+  "https://690ece994a3e6a3ba985e8d1--bookstore855.netlify.app",
+  "http://localhost:5173"
+];
 
-app.use(cors({
-    origin: allowedOrigins,
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
